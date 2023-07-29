de08dc1239da60faa37578eba850af8925f1b788
nairamangasaryan <mangasaryannaira@gmail.com>
2023-07-30 00:46:58 +0300

    Added login.html

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR /"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,4 +126,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL = reverse_lazy("home")
-
