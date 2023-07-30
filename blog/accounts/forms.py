8b68a8abbfd02e870d1242225e1715fc26d0d44c
nairamangasaryan <mangasaryannaira@gmail.com>
2023-07-30 17:54:33 +0300

    Added Signup and Login forms, registration and login of users

--- blog/accounts/forms.py
+++ blog/accounts/forms.py
@@ -18,15 +18,12 @@ class SignUpForm(UserCreationForm):
         fields = ["username", "email", "password1", "password2"]
 
 
-
-
 class LogInForm(UserCreationForm):
     username = forms.CharField(
         label="Username", widget=forms.TextInput(attrs={'class': 'form-input'}))
-   
     password = forms.CharField(
         label="Password", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    
+
     class Meta:
         model = User
         fields = ["username", "password"]
