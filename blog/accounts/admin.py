f1de835648b718b0b23061c93e10e0d89f14e30e
lilkhach <lilit.khachatryan.botany@gmail.com>
2023-08-01 23:04:51 +0400

    Adding signup

---
+++ blog/accounts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    name = 'accounts'
---
+++ blog/accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import SignUpForm
+from django.urls import reverse_lazy
+# Create your views here.
+
+
+class SignUp(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
+
+
+def finish(request):
+    return render(request, "registration/finish.html")
+
+
+def home(request):
+    return render(request, "registration/home.html")
