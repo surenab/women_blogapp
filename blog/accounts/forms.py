8234dbec149c0f19262003cd551e52b9236e26cd
lilkhach <lilit.khachatryan.botany@gmail.com>
2023-08-01 12:07:10 +0400

    adding updated form

--- blog/accounts/forms.py
+++ blog/accounts/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.utils.html import format_html
 
 
 class SignUpForm(UserCreationForm):
