5dacf0054a7743c8420f51ead5085c872148270d
nairamangasaryan <127053759+nairamangasaryan@users.noreply.github.com>
2023-08-04 22:38:00 +0400

    Update models.py
    Մարի ջան models.py ֆայլում որոշ ուղղումներ եմ արել։
    Եթե դեմ չես այս տարբերակով ավելացնենք պրոեկտին։

--- blog/core/models.py
+++ blog/core/models.py
@@ -2,19 +2,21 @@ from django.db import models
 from django.contrib .auth import get_user_model
 
 class Blog(models.Model):
-    # User= get_user_model
-
-    Blog_types= (
-       ( "1", "Travel"),
-       ("2", "Sport") ,
+  
+    BLOG_TYPES = (
+        ("1", "Travel"),
+        ("2", "Sport") ,
         ("3", "Natural"),
         ("4", "Funny animals"),
-        ("5", "nature"),
+        ("5", "nature")
     )
-    Blog_type = models.CharField(choices=Blog_types, default="1", max_length=1)
-    title= models.CharField(max_length=50)
     
-    create_data= models.DateField()
-    # username=models.ForeignKey(User, on_delete= models.CASCADE)
-    description= models.TextField()
-    image= models.ImageField( upload_to="Media", default=None, null=True, blank=True)
+    blog_type = models.CharField(choices=BLOG_TYPES, default="1", max_length=1)
+    title = models.CharField(max_length=50)
+    create_date = models.DateField()
+    user =models.ForeignKey(User, on_delete= models.CASCADE)
+    description = models.TextField()
+    image = models.ImageField(upload_to="Media", default=None, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
