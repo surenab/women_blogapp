54c66b68b49247d8808f48d137ad600ac080a3b9
Mariearakel <mari.arakelyan@gmail.com>
2023-08-04 20:33:16 +0400

    added models

--- blog/core/models.py
+++ blog/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib .auth import get_user_model
 
-# Create your models here.
+class Blog(models.Model):
+    # User= get_user_model
+
+    Blog_types= (
+       ( "1", "Travel"),
+       ("2", "Sport") ,
+        ("3", "Natural"),
+        ("4", "Funny animals"),
+        ("5", "nature"),
+    )
+    Blog_type = models.CharField(choices=Blog_types, default="1", max_length=1)
+    title= models.CharField(max_length=50)
+    
+    create_data= models.DateField()
+    # username=models.ForeignKey(User, on_delete= models.CASCADE)
+    description= models.TextField()
+    image= models.ImageField( upload_to="Media", default=None, null=True, blank=True)
