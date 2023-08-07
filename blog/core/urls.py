f22b71453965fec3008cf9f2945e3876d1d2dd7b
nairamangasaryan <mangasaryannaira@gmail.com>
2023-08-07 19:24:14 +0400

    Added fixtures

--- blog/core/urls.py
+++ blog/core/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import home, CreateBlog, single_post(, about, category, contact, search_result
+from .views import home, CreateBlog, single_post, about, category, contact, search_result
 
 
 urlpatterns = [
     path("", home, name="home"),
     path("createblog/", CreateBlog.as_view(), name="createblog"),
-    path("singlepost/", signle_post, name="singlepost"),
+    path("singlepost/", single_post, name="singlepost"),
     path("about/", about, name="about"),
     path("category/", category, name="category"),
     path("contact/", contact, name="contact"),
