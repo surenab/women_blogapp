cd10095f7b4edb1214c0332fefbf816a0d21f4d7
nairamangasaryan <mangasaryannaira@gmail.com>
2023-08-21 01:36:45 +0400

    Added count of viewes

--- blog/core/views.py
+++ blog/core/views.py
@@ -1,5 +1,7 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import BlogForm
 from .models import Blog
@@ -16,6 +18,7 @@ class Home(FilterView):
     context_object_name = "blogs"
     filterset_class = BlogFilter
     template_name = "core/home.html"
+
 
 class Base(LoginRequiredMixin, CreateView):
     def get_queryset(self):
@@ -39,12 +42,10 @@ class BlogBase(Base):
 class CreateBlog(BlogBase, CreateView):
     template_name = "core/create_blog.html"
 
-
     def form_valid(self, form):
         form.instance.user = self.request.user
         messages.success(self.request, "Blog was created successfully!")
         return super().form_valid(form)
-
 
 
 class MyBlog(LoginRequiredMixin, FilterView):
@@ -58,15 +59,14 @@ class MyBlog(LoginRequiredMixin, FilterView):
         queryset = super(MyBlog, self).get_queryset()
         queryset = queryset.filter(user=self.request.user)
         return queryset
-    
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        most_viewed_blogs = Blog.objects.order_by('-view_count')[:5] 
+        most_viewed_blogs = Blog.objects.order_by('-view_count')[:5]
         context['most_viewed_blogs'] = most_viewed_blogs
         return context
 
 
-      
 class MyBlogDetail(BlogBase, DetailView):
     template_name = "core/blog_detail.html"
 
@@ -75,6 +75,12 @@ class MyBlogDetail(BlogBase, DetailView):
         queryset = queryset.filter(user=self.request.user)
         return queryset
 
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        self.object = self.get_object()
+        self.object.view_count += 1
+        self.object.save()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
 
 
 class MyBlogUpdate(BlogBase, UpdateView):
